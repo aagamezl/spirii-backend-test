c97f5afa02d9152c99a96c1ef6f8d9b5b16bf515
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2025-04-05 20:24:19 +0200

    fix: adjust the rate limit update README

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ git clone https://github.com/aagamezl/spirii-backend-test
 cd spirii-backend-test
 npm install
 node src/index.js
+
+GET http://localhost:3000/users/074092/balance
+GET http://localhost:3000/users/payouts
 ```
--- src/server.js
+++ src/server.js
@@ -13,8 +13,8 @@ export const createServer = (aggregationService) => {
 
   // Rate limiting
   app.use(rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 1000 // limit each IP to 100 requests per windowMs
   }));
 
   // Proper route handlers as middleware functions
